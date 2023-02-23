b3b9bf92c41c74c7f5e2bae8b354fe148539e757
HelloworldA1 <86823813+HelloworldA1@users.noreply.github.com>
2023-02-23 19:25:22 +0800

    删除了代理服务器

--- vue.config.js
+++ vue.config.js
@@ -5,15 +5,15 @@ module.exports = defineConfig({
   //开启代理服务器
   devServer: {
     host: '0.0.0.0',
-    proxy: {
-      '/api':{
-        target: 'https://mineralsteins.icu:8080',
-        changeOrigin : true,
-        pathRewrite:{
-          '^/api':'',
-        },
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://mineralsteins.icu:8080',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': '',
+    //     },
+    //   },
+    // },
     // proxy : "https://mineralsteins.icu:8080"
 
   }
