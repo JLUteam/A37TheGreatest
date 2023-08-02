bf10a7a51293f1dd1698cd918fbc2c8e88727198
HelloworldA1 <m18357021209@163.com>
2023-08-02 16:21:47 +0800

    666

--- src/views/ConsumptionDetails_duotu.vue
+++ src/views/ConsumptionDetails_duotu.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="basic">
     <Back></Back>
-    <el-pagination layout="prev, pager, next" :total="recodes.length" @current-change="handleCurrentChange"
-      :page-size="1">
+    <el-pagination
+      layout="prev, pager, next"
+      :total="recodes.length"
+      @current-change="handleCurrentChange"
+      :page-size="1"
+    >
     </el-pagination>
     <Avatar :recode="this.recode_now"> </Avatar>
     <StateBar :recode="this.recode_now"></StateBar>
@@ -23,59 +27,54 @@ export default {
   name: "ConsumptionDetails",
   components: { Avatar, Back, StateBar, PayState, Another },
   mounted() {
-    this.recodes = this.$route.query.recode
+    this.recodes = this.$route.query.recode;
     this.recode_now = this.recodes[0];
-
   },
   data() {
     return {
       deleteorsave: "保存",
       recodes: [
-        {
-          "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
-          "bname": "武林川木桶饭",
-          "ispic": false,
-          "bpic": null,
-          "bcategory": "餐饮",
-          "note": "顾客需要餐具",
-          "payment": "支付宝",
-          "amount": 22.56,
-          "btime": "2023-02-15 17:47:15",
-          "isreceipt": true,
-          "receipt": null
-        },
-        {
-          "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
-          "bname": "状元狼食坊",
-          "ispic": false,
-          "bpic": null,
-          "bcategory": "餐饮",
-          "note": "特色凉菜(12元),招牌酸菜鱼,糖醋里嵴",
-          "payment": "支付宝",
-          "amount": 56.8,
-          "btime": "2023-06-05 17:25:14",
-          "isreceipt": true,
-          "receipt": null
-        },
-
-        {
-          "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
-          "bname": "农家木桶饭",
-          "ispic": false,
-          "bpic": null,
-          "bcategory": "餐饮",
-          "note": "四季豆炒肉丝木桶饭",
-          "payment": "支付宝",
-          "amount": 17.30,
-          "btime": "2023-04-02 12:55:34",
-          "isreceipt": true,
-          "receipt": null
-        },
-
-
-
+        // {
+        //   "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
+        //   "bname": "武林川木桶饭",
+        //   "ispic": false,
+        //   "bpic": null,
+        //   "bcategory": "餐饮",
+        //   "note": "顾客需要餐具",
+        //   "payment": "支付宝",
+        //   "amount": 22.56,
+        //   "btime": "2023-02-15 17:47:15",
+        //   "isreceipt": true,
+        //   "receipt": null
+        // },
+        // {
+        //   "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
+        //   "bname": "状元狼食坊",
+        //   "ispic": false,
+        //   "bpic": null,
+        //   "bcategory": "餐饮",
+        //   "note": "特色凉菜(12元),招牌酸菜鱼,糖醋里嵴",
+        //   "payment": "支付宝",
+        //   "amount": 56.8,
+        //   "btime": "2023-06-05 17:25:14",
+        //   "isreceipt": true,
+        //   "receipt": null
+        // },
+        // {
+        //   "usr": "780303f9-b0a1-4d7b-a7b4-d191daa85f47",
+        //   "bname": "农家木桶饭",
+        //   "ispic": false,
+        //   "bpic": null,
+        //   "bcategory": "餐饮",
+        //   "note": "四季豆炒肉丝木桶饭",
+        //   "payment": "支付宝",
+        //   "amount": 17.30,
+        //   "btime": "2023-04-02 12:55:34",
+        //   "isreceipt": true,
+        //   "receipt": null
+        // },
       ],
-      recode_now: {}
+      recode_now: {},
     };
   },
   methods: {
@@ -84,40 +83,67 @@ export default {
     },
 
     delete_() {
-      this.$confirm('是否保存', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning',
-        customClass: 'msgbox'
-      }).then(() => {
-        //从recodes中删除recode_now
-        this.recodes.splice(this.recodes.indexOf(this.recode_now), 1);
-        console.log(1232135)
-        console.log(this.recodes.length);
-        if (this.recodes.length == 0) {
-          this.$router.push("/Percent");
-        }
-      }).catch(() => {
-        // this.$message({
-        //   type: 'info',
-        //   message: '已取消删除'
-        // });
-      });
+      this.$confirm("是否保存", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+        customClass: "msgbox",
+      })
+        .then(() => {
+          //从recodes中删除recode_now
+          console.log("-------come in-------");
+          console.log(this.recodes);
+          console.log(this.recode_now);
+          var id =
+            this.$store.state.recodes
+              .map((item) => item.id)
+              .reduce((a, b) => Math.max(a, b)) + 1;
+          var usr = this.$store.state.userinfo.uid;
+          var datatolocal = {
+            bname: this.recode_now.bname,
+            bcategory: this.recode_now.bcategory,
+            note: this.recode_now.note,
+            payment: this.recode_now.payment,
+            amount: this.recode_now.amount,
+            btime: this.recode_now.btime,
+            isreceipt: this.recode_now.isreceipt,
+            receipt: this.recode_now.receipt,
+            ispic: true,
+            bpic: null,
+            usr: usr,
+            id: id,
+          };
+          console.log("datatolocal:");
+          console.log(datatolocal);
+          this.recodes.splice(this.recodes.indexOf(this.recode_now), 1);
+          console.log(1232135);
+          console.log(this.recodes);
+          console.log(this.recode_now);
+          console.log(this.recodes.length);
+          if (this.recodes.length == 0) {
+            this.$router.push("/Percent");
+          }
+        })
+        .catch(() => {
+          // this.$message({
+          //   type: 'info',
+          //   message: '已取消删除'
+          // });
+        });
     },
     isupdate() {
       this.deleteorsave = "保存";
-
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
       this.recode_now = this.recodes[val - 1];
-    }
+    },
   },
   watch: {
     recode_now: {
       handler: function (val, oldval) {
         console.log("recode_now");
-        console.log(this.recode_now)
+        console.log(this.recode_now);
         console.log(val);
       },
       deep: true,
@@ -130,9 +156,8 @@ export default {
       },
       deep: true,
       immediate: true,
-    }
-
-  }
+    },
+  },
 };
 </script>
 
@@ -142,8 +167,7 @@ export default {
 }
 </style>
 
-
-<style lang='less' scoped>
+<style lang="less" scoped>
 .basic {
   display: flex;
   flex-direction: column;
@@ -171,10 +195,9 @@ export default {
     margin-top: 0.32rem;
   }
 
-
   /deep/.el-pagination {
-    margin-left: .2rem;
-    margin-top: .6rem;
+    margin-left: 0.2rem;
+    margin-top: 0.6rem;
     background-color: transparent;
   }
 
@@ -192,17 +215,12 @@ export default {
   }
 
   /deep/.el-pagination .btn-next {
-
     display: none;
     background-color: transparent;
   }
 
   .avatar {
-    margin-top: .2rem;
+    margin-top: 0.2rem;
   }
-
-
-
-
 }
 </style>
